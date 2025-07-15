96aa6d7c1a43e2a607a1bc6cd484af88838874b4
Daniel Choi <danielchoi1115@gmail.com>
2025-07-15 17:52:36 +0900

    first commit

--- src/main/java/com/winten/greenlight/thehyundaisample/ThehyundaisampleApplication.java
+++ src/main/java/com/winten/greenlight/thehyundaisample/ThehyundaisampleApplication.java
@@ -2,9 +2,15 @@ package com.winten.greenlight.thehyundaisample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThehyundaisampleApplication {
+public class ThehyundaisampleApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ThehyundaisampleApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ThehyundaisampleApplication.class, args);
--- src/test/java/com/winten/greenlight/thehyundaisample/ThehyundaisampleApplicationTests.java
+++ src/test/java/com/winten/greenlight/thehyundaisample/ThehyundaisampleApplicationTests.java
@@ -2,10 +2,9 @@ package com.winten.greenlight.thehyundaisample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootTest
-class ThehyundaisampleApplicationTests extends SpringBootServletInitializer {
+class ThehyundaisampleApplicationTests {
 
     @Test
     void contextLoads() {
