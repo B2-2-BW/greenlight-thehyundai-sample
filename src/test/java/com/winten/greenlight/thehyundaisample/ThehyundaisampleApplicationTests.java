fd153a98f5b82aa535e02009064e22a9bc5b23b4
Daniel Choi <danielchoi1115@gmail.com>
2025-07-15 17:49:11 +0900

    first commit

--- src/test/java/com/winten/greenlight/thehyundaisample/ThehyundaisampleApplicationTests.java
+++ src/test/java/com/winten/greenlight/thehyundaisample/ThehyundaisampleApplicationTests.java
@@ -2,9 +2,10 @@ package com.winten.greenlight.thehyundaisample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootTest
-class ThehyundaisampleApplicationTests {
+class ThehyundaisampleApplicationTests extends SpringBootServletInitializer {
 
     @Test
     void contextLoads() {
