8e562dd6277014d5a125badbdcf8b4af2e59ef0a
gimminjeong <lovelyminjeong91@gmail.com>
2025-07-24 21:17:09 +0900

    fix: WebConfig 순환 참조 문제 해결 및 코드 정리\n\n- WebConfig와 CachedActionService 간의 순환 참조 문제를 해결함.\n- WebConfig에서 SampleInterceptor를 직접 주입받아 사용하도록 수정함.\n- 불필요한 CachedActionService 의존성 및 SampleInterceptor 빈 생성 관련 주석 코드를 제거하여 코드 가독성을 높임.

--- src/main/java/com/winten/greenlight/thehyundaisample/config/WebConfig.java
+++ src/main/java/com/winten/greenlight/thehyundaisample/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.winten.greenlight.thehyundaisample.config;
 
 import com.winten.greenlight.thehyundaisample.interceptor.SampleInterceptor;
-import com.winten.greenlight.thehyundaisample.service.CachedActionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final CachedActionService cachedActionService;
-    private final SampleInterceptor sampleInterceptor; // SampleInterceptor를 주입받도록 추가
+    private final SampleInterceptor sampleInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //Spring이 관리하는 SampleInterceptor 빈을 직접 주입받아 사용합니다.
-        // 직접 주입받은 sampleInterceptor 사용
         registry.addInterceptor(sampleInterceptor).addPathPatterns("/**");
     }
 
@@ -27,10 +23,4 @@ public class WebConfig implements WebMvcConfigurer {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
-//    @Bean
-//    public SampleInterceptor sampleInterceptor() {
-//        // 인터셉터 Bean 생성 시, 필요한 의존성을 직접 주입합니다.
-//        return new SampleInterceptor(restTemplate(), cachedActionService);
-//    }
 }
