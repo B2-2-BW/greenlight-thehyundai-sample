e4b3c322f53025eedbaf7f9cd10dbeb2530caf43
Daniel Choi <danielchoi1115@gmail.com>
2025-08-06 17:32:16 +0900

    랜딩타입 토큰처리 추가

--- src/main/java/com/winten/greenlight/thehyundaisample/greenlight/GreenlightConstant.java
+++ src/main/java/com/winten/greenlight/thehyundaisample/greenlight/GreenlightConstant.java
@@ -3,5 +3,5 @@ package com.winten.greenlight.thehyundaisample.greenlight;
 public class GreenlightConstant {
     public static final String GREENLIGHT_API_KEY = "X-GREENLIGHT-API-KEY";
     public static final String GREENLIGHT_TOKEN = "X-GREENLIGHT-TOKEN";
-    public static final String GREENLIGHT_PARAM_TOKEN = "gt";
+    public static final String GREENLIGHT_PARAM_TOKEN = "g";
 }
